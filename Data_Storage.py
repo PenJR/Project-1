43630552b7bbbf7e1470e369a5adf419b7e6af30
PenJR <152262578+PenJR@users.noreply.github.com>
2024-11-29 06:53:47 +0800

    Update Data_Storage.py

--- Data_Storage.py
+++ Data_Storage.py
@@ -1,44 +1,75 @@
-import json 
+import json
 from Musiclib import MusicLibrary
 from Track import Track
 from Playlist import Playlist
 
 class DataStorage:
-  @staticmethod
-  def save(library,playlists):
-    data = {
-      "Music_Library": [track.__dict__ for track in library.get_tracks()],
-      "Playlists" : { playlist.name : [track.__dict__ for track in playlist.get_tracks()]
-                    for playlist in playlists
-                   }
-      }
-     
-    with open ('MusicData.json','w') as file:
-      json.dump(data,file)
-      
-  @staticmethod
-  def load():
-    try:
-      with open ('MusicData.json','r') as file:
-        data = json.load(file)
-        Library = MusicLibrary()
-        playlists = []
-      
-        for track_data in data["music_library"]:
-            track = Track(track_data["title"], track_data["artist"], track_data["duration"])
-            Library.add_track(track)
+    
+    @staticmethod
+    def save_music_library(library):
+        data = {
+            "Music_Library": [track.__dict__ for track in library.get_tracks()]
+        }
+        
+        with open('MusicData.json', 'w') as file:
+            json.dump(data, file)
+    
+    @staticmethod
+    def load_music_library():
+        try:
+            with open('MusicData.json', 'r') as file:
+                data = json.load(file)
+                library = MusicLibrary()
+
+                for track_data in data["Music_Library"]:
+                    track = Track(track_data["title"], track_data["artist"], track_data["duration"])
+                    library.add_track(track)
+                    
+                return library
                 
-             
-        for playlist_name, track_data_list in data["playlists"].items():
-             playlist = Playlist(playlist_name)
+        except FileNotFoundError:
+            return MusicLibrary()
+    
+    @staticmethod
+    def save_playlists(playlists):
+        data = {
+            "Playlists": {
+                playlist.name: [track.__dict__ for track in playlist.get_tracks()]
+                for playlist in playlists
+            }
+        }
+        
+        with open('MusicData.json', 'w') as file:
+            json.dump(data, file)
+    
+    @staticmethod
+    def load_playlists():
+        try:
+            with open('MusicData.json', 'r') as file:
+                data = json.load(file)
+                playlists = []
 
-        for track_data in track_data_list:
-                track = Track(track_data["title"], track_data["artist"], track_data["duration"])
-              # playlist.add_track(track)
-        playlists.append(playlist)
+                for playlist_name, track_data_list in data["Playlists"].items():
+                    playlist = Playlist(playlist_name)
+                    
+                    for track_data in track_data_list:
+                        track = Track(track_data["title"], track_data["artist"], track_data["duration"])
+                        playlist.add_track(track)
+                    
+                    playlists.append(playlist)
+                    
+                return playlists
 
-        return Library, playlists
-      
-    except FileNotFoundError:
-        return MusicLibrary(), []
-
+        except FileNotFoundError:
+            return []
+    
+    @staticmethod
+    def save(library, playlists):
+        DataStorage.save_music_library(library)
+        DataStorage.save_playlists(playlists)
+    
+    @staticmethod
+    def load():
+        library = DataStorage.load_music_library()
+        playlists = DataStorage.load_playlists()
+        return library, playlists
