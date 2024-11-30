032c5dc63d569ee7331bed5c603c9d527e89d8c3
PenJR <152262578+PenJR@users.noreply.github.com>
2024-11-30 15:26:04 +0800

    Update Data_Storage.py

--- Data_Storage.py
+++ Data_Storage.py
@@ -3,27 +3,27 @@ import json
 class DataStorage:
 #MUSIC Library and Playlist
     #SAVE
-    def save_music(music_data,filename):
+    def save_music(music_data):
         try:
-            with open(filename, 'w') as json_file:
+            with open("Music_Data.json", 'w') as json_file:
                 json.dump(music_data, json_file, indent=4)  
-            print(f"Playlist saved successfully to {filename}")
+            print(f"Playlist saved successfully to Music_Data}")
         except Exception as e:
             print(f"Error saving playlist: {e}")
 
     #LOAD
-    def load_music(filename):
+    def load_music(self):
         try:
           
-            with open(filename, 'r') as json_file:
+            with open("Music_Data.json", 'r') as json_file:
                 playlist_data = json.load(json_file)  
-            print(f"Playlist loaded successfully from {filename}")
+            print(f"Playlist loaded successfully from Music_Data.json")
             return playlist_data  
         except FileNotFoundError:
-            print(f"Error: The file {filename} was not found.")
+            print(f"Error: The file Music_Data.json was not found.")
             return None
         except json.JSONDecodeError:
-            print(f"Error: Failed to decode JSON in playlist.json. It might be corrupted or not properly formatted.")
+            print(f"Error: Failed to decode JSON in Music_Data.json, It might be corrupted or not properly formatted.")
 
 
 
