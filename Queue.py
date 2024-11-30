de6776de5f4fac129fbcf62f36cad75a616ff0de
hattie <michaelaamormanalo@gmail.com>
2024-11-30 14:38:04 +0800

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -92,41 +92,6 @@ class Queue:
             track = self.list[i]
             print(f"{prefix} {track.title} - {track.artist} ({track.duration})")
 
-    def save_queue(self):
-    
-        try:
-            with open("queue.json", "r") as file:
-                existing_tracks = json.load(file)
-        except (FileNotFoundError, json.JSONDecodeError):
-            existing_tracks = []
-
-        current_tracks = [
-            {"title": track.title, "artist": track.artist, "duration": track.duration}
-            for track in self.list
-        ]
-        existing_tracks.extend(current_tracks)
-
-        with open("queue.json", "w") as file:
-            json.dump(existing_tracks, file, indent=4)
-
-        print("Queues saved.")  
-
-
-    def load_queue(self):
-        try:
-            with open("queue.json", "r") as file:
-                tracks = json.load(file)
-
-            self.list = [
-                Track(track["title"], track["artist"], track["duration"]) for track in tracks
-            ]
-            self.total_duration = sum(track["duration"] for track in tracks)
-            self.current_index = 0 if self.queue else None
-            print("Queues loaded.")
-        except FileNotFoundError:
-            print("No saved queue found.")
-        except json.JSONDecodeError:
-            print("Error decoding JSON. Queue file might be corrupted.")
 
     def exit(self):
         self.save_queue()
