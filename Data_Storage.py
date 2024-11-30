c5591e6c817358cd8ec3b31237e166b2013c31f4
PenJR <152262578+PenJR@users.noreply.github.com>
2024-11-30 15:31:26 +0800

    Update Data_Storage.py

--- Data_Storage.py
+++ Data_Storage.py
@@ -26,7 +26,6 @@ class DataStorage:
             print(f"Error: Failed to decode JSON in Music_Data.json, It might be corrupted or not properly formatted.")
 
     def save_playlist(self, file_path="Playlist.json"):
-        """Save the playlist to a JSON file."""
         try:
             with open(file_path, "r") as f:
                 data = json.load(f)
@@ -54,6 +53,39 @@ class DataStorage:
             print(f"Playlist '{self.name}' saved successfully.")
         except Exception as e:
             print(f"Error saving playlist: {e}")
+
+    @staticmethod
+    def load_playlist(name, file_path="Playlist.json"):
+      """Load a playlist by name from a JSON file."""
+      try:
+        with open(file_path, "r") as f:
+            data = json.load(f)
+      except FileNotFoundError:
+        print(f"Error: The file '{file_path}' does not exist.")
+        return None
+      except json.JSONDecodeError:
+        print(f"Error: The file '{file_path}' contains invalid JSON.")
+        return None
+
+
+      if name in data:
+        playlist_data = data[name]
+        playlist = Playlist(playlist_data["Playlist Name"])
+
+
+        playlist.tracks = [
+            Track.from_dict(track_data)
+            for track_data in playlist_data.get("Tracks", [])
+        ]
+
+   
+        playlist.total_duration = playlist_data.get("Total Duration", 0)
+
+        print(f"Playlist '{name}' loaded successfully.")
+        return playlist
+      else:
+        print(f"Playlist '{name}' not found in '{file_path}'.")
+        return None
 
 
     
