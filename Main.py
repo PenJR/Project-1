6a80f102d71debd6a6b99d4d0a1ce8b98be00475
SharkAC <aaronclydeccervantes@gmail.com>
2024-11-30 13:10:36 +0800

    Update Main.py

---
+++ Main.py
@@ -0,0 +1,37 @@
+import json
+from Music_Library import MusicLibrary
+from Playlist import Playlist
+from Track import Track
+from Data_Storage import DataStorage
+import random
+
+# MENUS dictionary
+MENUS = {
+    "main": {
+        1: "Play Music",
+        2: "Music Library",
+        3: "Playlists",
+        4: "Exit"
+    },
+    "Music Player": {
+        1: "Play",
+        2: "Next",
+        3: "Previous",
+        4: "Toggle Repeat",
+        5: "Toggle Shuffle",
+        6: "Clear Queue",
+        7: "Exit"
+    },
+    "Music Library": {
+        1: "Add Track",
+        2: "View Tracks",
+        3: "Search Tracks",
+        4: "Go Back to Main Menu"
+    },
+    "Playlist": {
+        1: "Create Playlist",
+        2: "View Playlists",
+        3: "Add Track to Playlist",
+        4: "Go Back to Main Menu"
+    }
+}
