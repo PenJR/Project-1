785904627d6fe2510959b86eefc5ab2ee6e34cfc
SharkAC <aaronclydeccervantes@gmail.com>
2024-11-30 11:40:48 +0800

    Track

--- Track.py
+++ Track.py
@@ -1,1 +1,14 @@
-Track
+class Track:
+    def __init__(self, title, artist, album, duration):
+        """
+        Initializes a new Track object.
+
+        :param title: The title of the track
+        :param artist: The artist of the track
+        :param album: The album of the track
+        :param duration: The duration of the track in "mm:ss" format
+        """
+        self.title = title
+        self.artist = artist
+        self.album = album
+        self.duration = duration
